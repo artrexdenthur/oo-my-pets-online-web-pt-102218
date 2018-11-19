a3092bdcd54191d24dfae6cd1503976103f7f12d
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 16:33:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -19,6 +19,14 @@ class Owner
     @pets[:fishes] << Fish.new(name)
   end
   
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+  
   ############### Class Methods ################
   
   def self.all
